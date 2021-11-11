d64f30dde775b0e89284a9ec1941484a91a3a578
SatoshiBitcoin <office@satoshibitcoin.app>
2021-11-11 16:06:27 +0100

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,91 @@
+<?php
+
+function rpc_get($post_json) {
+	
+	// Daca nu te poti conecta trebuie sa verifici fisierul de setari /home/max/.satoshibitcoin/satoshibitcoin.conf sa aiba urmatoarele 2 linii
+	//  rpcallowip=0.0.0.0/0
+	//  main.rpcbind=0.0.0.0
+	
+	// exemplu de accesare cu curl
+	// curl --user <username>:<password> --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "getrawtransaction", "params": ["3a694df52e674e73d584bd455224b1afbb898d8a56c4c7bd80f6b660b05d8852", true]}' -H 'content-type: text/plain;' http://173.249.52.171:5332/
+	
+	
+	$result = false; // se va schimba pe parcurs
+
+	$RPCusername = '<username>';
+	$RPCpassword = '<password>';
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_setopt($ch, CURLOPT_USERPWD, $RPCusername.':'.$RPCpassword);
+	curl_setopt($ch, CURLOPT_URL, 'http://server2.satoshibitcoin.app:5332'); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	#curl_setopt($ch, CURLOPT_POSTFIELDS, '{"id":"0","method":"getblock","params":{"address":"00000002f563882865e73dd81e80a37016943d1d403785e65d6080d44903289e"}}'); 
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
+
+	curl_setopt($ch, CURLOPT_POST, 1);
+
+	$headers = array(); 
+	$headers[] = "Content-Type: application/x-www-form-urlencoded"; 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	$result = curl_exec($ch); 
+	if (curl_errno($ch)) { 
+		echo 'Error:'.curl_error($ch).PHP_EOL; 
+	} 
+	else {
+//		echo $result;
+		$result =json_decode($result,true)["result"]; // decodam json-ul primit de la server direct in array (si copiem informatia din ['result'])
+
+	}
+	curl_close($ch);
+	
+	return $result;
+}
+
+function convert_hashes_to_links($input) { /*convertim ID-urile la blockuri/adrese in linkuri*/
+   $pattern = '@\b\w{64}\b@';
+   return $output = preg_replace($pattern, '<a rel="nofollow" href="/explorer/?search=$0">$0</a>', $input);
+}	
+	
+function print_r_table($arr) {  // afisam array-ul pe ecran
+	
+	// stergem caracterele din fata si din spatele la rezultatul functiei print_r()
+	$str = print_r($arr, true);
+
+	// stergem Array-ul
+	$str = substr($str, 8);   // stergem cuvantul 'Array ()' din fata
+	$str = substr($str, 0, -3);   // stergem paranteza de la sfarsit ')' din fata
+
+	// stergem array-ul de prin interior subnivele
+//	$str = str_replace("Array\n\r(", '', $str);
+//	$str = preg_replace("/Array[\s\n\r\(]/i", '', $str);
+//	$str = str_replace("    )\n", '', $str);
+
+	// stergem la toate liniile patru spatii de la inceput daca exista
+	$arr_lines = explode("\n",$str);
+	foreach($arr_lines as &$line) {    
+		if (substr($line, 0, 4)=='    ') $line = substr($line, 4); // stergem primele 4 caracrere din linie daca sunt 4 spatii
+	}
+	$str=implode("\n", $arr_lines);
+	
+	$str = str_replace("[tx] => Array", '[tx] => <b>Transactions:</b>', $str);
+	$str = str_replace("[", '', $str);
+	$str = str_replace("]", '', $str);
+	$str = str_replace("=>", '=', $str);
+	$str = str_replace("Array", '', $str);	
+
+//	$str = str_replace("(", '', $str);
+//	$str = str_replace(")", '', $str);
+
+	
+	
+	
+
+	// Afisem print_r() nostru
+	echo '<div style="width:700px; display:block; margin-left:auto; margin-right:auto;">';
+	echo '<pre>';
+	echo  convert_hashes_to_links($str);
+	echo '</pre>';
+	echo '</div>';
+}
+?>
---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+	ob_start("ob_gzhandler");
+
+
+	if (isset($_GET['search'])) { // Daca suntem intr-un url (daca exista parametrul &search)
+		// in orice alta pagina inafara de homepage (avem cache la pagina o ora)
+		$seconds_to_cache = 86400; // 60*60*24 // 24 de ore
+		$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+		header("Expires: $ts");
+		header("Pragma: cache");
+		header("Cache-Control: max-age=$seconds_to_cache");
+
+		header("HTTP/1.0 404 Not Found"); // daca exista parametrul &search in url facem 404 (page not found) sa nu se indexeze multe paginii aiurea
+	} else { // daca suntem in homepage
+		// in homepage avem header no cache
+		header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1        // Nu tine chace pt pagini dinamice
+		header("Expires: Mon, 27 Jul 2011 07:08:02 GMT");   // Date in the past  // Nu tine chace pt pagini dinamice
+	}
+	
+	include('functions.php');
+?>
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<?php if (isset($_GET['search'])) echo '<meta name="robots" content="noindex">'; // daca exista parametrul &search in url facem noindex sa nu se indexeze multe paginii aiurea ?>
+<title>SathosiBitcoin Block Explorer</title>
+<link rel="canonical" href="<?php echo $_SERVER['SCRIPT_URI'] ?>" />
+<link rel="shortcut icon" href="favicon.svg" />	
+<link rel="stylesheet" href="style.css" type="text/css" />	
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+
+	
+</head>
+
+<body>
+	
+<center>
+
+	<a href="/" style="color:black; text-decoration: none; width: 500px; display:block" title="Homepage">
+		<img style="vertical-align:bottom;" src="/assets/img/favicon.png" height="60">
+	</a>
+	
+	
+	<a href="/explorer" style="color:black; text-decoration: none; width: 500px; display:block">
+		<h1 style="margin-top:40px; margin-bottom:40px; "><img style="vertical-align:bottom;" src="favicon.svg" height="60"> SathosiBitcoin Block Explorer</h1>
+	</a>
+
+	<form method="get">
+	  <label for="search"><input name="search" type="search" id="search" placeholder="Search hash or height" value="<?php echo htmlentities(trim($_GET['search'])) ?>"></label>
+	  <input type="submit" id="search_button" value="Search">
+	</form>
+
+</center>
+	
+	
+<?php
+
+
+
+	  
+	  
+$search = trim($_GET['search']);
+	
+if ($search=='') { // Daca suntem in homepage (daca nu sa cautat nimic)
+	echo '<br><br><center>Best Block</center>';
+//	$getblockchaininfo_arr = rpc_get('{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo" }');
+	$bestblockhash = rpc_get('{"jsonrpc": "1.0", "id":"curltest", "method": "getbestblockhash" }');
+	$block_arr             = rpc_get('{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["'.$bestblockhash.'"] }');
+	if ($block_arr['hash']!='') { // daca am gasit block-ul
+		print_r_table($block_arr);
+		echo '<center>';
+		if ($block_arr['nextblockhash']!='') echo '<a rel="nofollow" href="/explorer/?search='.$block_arr['nextblockhash'].'" style="color:black; text-decoration: none;">&lt;&lt;&lt; next</a> &nbsp; ';
+		if ($block_arr['previousblockhash']!='') echo '<a rel="nofollow" href="/explorer/?search='.$block_arr['previousblockhash'].'" style="color:black; text-decoration: none;">previous &gt;&gt;&gt;</a>'; 
+		echo '</center>';
+	}
+} else { // daca sa cautat ceva
+
+	// cautam blocul
+	$block_arr = rpc_get('{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["'.$search.'"] }');
+	if ($block_arr['hash']!='') { // daca am gasit block-ul
+		echo '<br><br><center>Block</center>';
+		print_r_table($block_arr);
+		echo '<center>';
+		if ($block_arr['nextblockhash']!='') echo '<a rel="nofollow" href="/explorer/?search='.$block_arr['nextblockhash'].'" style="color:black; text-decoration: none;">&lt;&lt;&lt; next</a> &nbsp; ';
+		if ($block_arr['previousblockhash']!='') echo '<a rel="nofollow" href="/explorer/?search='.$block_arr['previousblockhash'].'" style="color:black; text-decoration: none;">previous &gt;&gt;&gt;</a>'; 
+		echo '</center>';
+
+	}
+	else { // Daca nu exista blocul cautam adresa tranzactiei
+		$transaction_arr = rpc_get('{"jsonrpc": "1.0", "id":"curltest", "method": "getrawtransaction", "params": ["'.$search.'", true] }');
+		if ($transaction_arr['txid']!='') { // daca am gasit tranzactia
+			echo '<br><br><center>Transaction</center>';
+			print_r_table($transaction_arr);
+		}
+	}
+
+}
+
+	  
+	
+
+	
+
+	  
+
+?>
+<br><br><br><br>
+</body>
+</html>
